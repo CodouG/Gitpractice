25fe8d3f033c428e85e276cf0c7682827a90e92c
Codou Gueye <codoumbakhe@gmail.com>
2025-07-23 12:02:25 -0600

    Initial Commit

---
+++ Gitpractice/GitpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitpracticeApp.swift
+//  Gitpractice
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GitpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
